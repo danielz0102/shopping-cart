f7b1bdfdc975de6309b00da6ae3854ac870f9fb4
danielz0102 <danyzglez@hotmail.com>
2025-04-26 14:53:22 -0600

    chore: add initial files

---
+++ README.md
@@ -0,0 +1,13 @@
+# Shopping Cart
+
+## TODO
+
+- [x] Install and config dependencies
+  - [x] Prettier
+  - [x] Testing Library
+  - [x] React Router
+- [x] Set up folder structure
+- [x] Add reset CSS file
+- [] Fetch items from [FakeStore API](https://fakestoreapi.com/)
+- [] Create UI
+- [] Deploy
---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+  test: {
+    environment: 'jsdom',
+    setupFiles: './tests/setup.js',
+  },
+})
