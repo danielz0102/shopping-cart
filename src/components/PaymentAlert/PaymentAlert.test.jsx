61c1e61ac105f7e9ca8ad9ff2e056cee31f1fa67
danielz0102 <danyzglez@hotmail.com>
2025-04-30 00:47:21 -0600

    feat: add PaymentAlert component

---
+++ src/components/PaymentAlert/PaymentAlert.test.jsx
@@ -0,0 +1,37 @@
+import { test, expect, vi } from 'vitest'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+
+import PaymentAlert from '.'
+
+test('onConfirm is a function', () => {
+  expect(() => render(<PaymentAlert />)).toThrow()
+  expect(() => render(<PaymentAlert onConfirm={123} />)).toThrow()
+  expect(() => render(<PaymentAlert onConfirm={'123'} />)).toThrow()
+  expect(() => render(<PaymentAlert onConfirm={{}} />)).toThrow()
+})
+
+test('renders open', () => {
+  render(<PaymentAlert onConfirm={() => {}} />)
+
+  screen.getByRole('heading')
+})
+
+test('closes on click close button', async () => {
+  const user = userEvent.setup()
+  render(<PaymentAlert onConfirm={() => {}} />)
+
+  await user.click(screen.getByRole('button', { name: /close/i }))
+
+  expect(screen.queryByRole('heading')).not.toBeInTheDocument()
+})
+
+test('executes onConfirm when confirm button is clicked', async () => {
+  const onConfirm = vi.fn()
+  const user = userEvent.setup()
+  render(<PaymentAlert onConfirm={onConfirm} />)
+
+  await user.click(screen.getByRole('button', { name: /confirm/i }))
+
+  expect(onConfirm).toHaveBeenCalled()
+})
--- src/components/PaymentAlert/index.jsx
+++ src/components/PaymentAlert/index.jsx
@@ -1,3 +1,19 @@
-export default function PaymentAlert() {
-  return <h2>Payment Alert</h2>
+import { useState } from 'react'
+
+export default function PaymentAlert({ onConfirm }) {
+  const [open, setOpen] = useState(true)
+
+  if (typeof onConfirm !== 'function') {
+    throw new Error('onConfirm must be a function')
+  }
+
+  return (
+    <dialog open={open}>
+      <h1>Confirm payment</h1>
+      <button aria-label="Close" onClick={() => setOpen((prev) => !prev)}>
+        x
+      </button>
+      <button onClick={onConfirm}>Confirm</button>
+    </dialog>
+  )
 }
