24544a7639f1ccd196446abea4ae9db6dfeb73f1
danielz0102 <danyzglez@hotmail.com>
2025-05-05 23:31:10 -0600

    feat: add Inter font

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,5 @@
+// Supports weights 100-900
+import '@fontsource-variable/inter'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
