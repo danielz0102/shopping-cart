7927db095b21413ffd495db1196f8f3e04efc244
danielz0102 <danyzglez@hotmail.com>
2025-05-11 19:17:37 -0600

    test(Counter): add unit tests

--- src/components/UI/Counter/index.jsx
+++ src/components/UI/Counter/index.jsx
@@ -1,14 +1,20 @@
 import styles from './Counter.module.css'
+import z from 'zod'
 
 import { useId } from 'react'
 
 import { Minus, Plus } from 'lucide-react'
 
-export default function Counter({
-  onClickDecrement,
-  onClickIncrement,
-  inputAttributes = {},
-}) {
+const propsSchema = z.object({
+  onClickDecrement: z.function(),
+  onClickIncrement: z.function(),
+  inputAttributes: z.record(z.any()).default(() => ({})),
+})
+
+export default function Counter(props) {
+  const { onClickDecrement, onClickIncrement, inputAttributes } =
+    propsSchema.parse(props)
+
   const id = useId()
   return (
     <div className={styles.counter}>
