5b81e809c319bdd0dea937134cd4c789833b3d59
danielz0102 <danyzglez@hotmail.com>
2025-05-16 20:23:20 -0600

    feat(PaymentAlert): add backdrop effect

--- src/components/Checkout/components/PaymentAlert/index.jsx
+++ src/components/Checkout/components/PaymentAlert/index.jsx
@@ -1,5 +1,7 @@
 import styles from './PaymentAlert.module.css'
 
+import { useEffect, useRef } from 'react'
+
 import Button from '@/components/UI/Button'
 import CloseBtn from '@/components/UI/CloseBtn'
 import { Check } from 'lucide-react'
@@ -17,8 +19,14 @@ export default function PaymentAlert({ open = true, onConfirm, onClose }) {
     throw new Error('onClose must be a function')
   }
 
+  const alert = useRef()
+
+  useEffect(() => {
+    alert.current[open ? 'showModal' : 'close']()
+  }, [open])
+
   return (
-    <dialog className={styles.alert} open={open}>
+    <dialog className={styles.alert} ref={alert}>
       <h1>Confirm payment</h1>
       <CloseBtn className={styles.closeButton} onClick={onClose} />
       <Button className={styles.confirmButton} onClick={onConfirm}>
