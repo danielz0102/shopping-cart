0b7026ba5b557f702e5b14596fe936b2808ef222
danielz0102 <danyzglez@hotmail.com>
2025-05-18 19:06:57 -0600

    feat(Shop): improve loading state

--- src/components/Shop/index.jsx
+++ src/components/Shop/index.jsx
@@ -1,27 +1,32 @@
 import styles from './Shop.module.css'
+import spinner from '@/assets/loader.svg'
 import { useProducts } from '@/hooks/useProducts'
 import Product from '../Product'
 
 export default function Shop() {
   const { products, loading, error } = useProducts()
 
-  if (loading) {
-    return <h1>Loading...</h1>
-  }
-
   if (error) {
     return <h1>Oops... something went wrong</h1>
   }
 
-  if (products.length === 0) {
+  if (products.length === 0 && !loading) {
     return <h1>No products found</h1>
   }
 
   return (
     <main className={styles.shop}>
-      {products.map((product) => (
-        <Product key={product.id} product={product} addToCart={() => {}} />
-      ))}
+      {loading ? (
+        <div className={styles.spinnerContainer}>
+          <img src={spinner} alt="Loading..." />
+        </div>
+      ) : (
+        <div className={styles.itemsGrid}>
+          {products.map((product) => (
+            <Product key={product.id} product={product} addToCart={() => {}} />
+          ))}
+        </div>
+      )}
     </main>
   )
 }
